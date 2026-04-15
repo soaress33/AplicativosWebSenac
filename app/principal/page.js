39bba6a88ab60d628018a7f9a2436056f3805cb8
soaress33 <1036603@senacminas.edu.br>
2026-04-14 21:43:38 -0300

    tela principal]

--- app/principal/page.js
+++ app/principal/page.js
@@ -5,23 +5,25 @@ export default function Home() {
   return (
 
 
-    <div className="bg-black items-center  w-screen h-screen">
+    <div className=" bg-no-repeat bg-cover bg-center items-center  w-screen h-screen" 
+    style={{backgroundImage: "url('/fundo.svg')"}}>
+
       <div className="grid grid-cols-[300px_1fr]  h-screen">
-        <div className="bg-orange-400 text-3xl flex flex-col row-span-1 p-5">
-          <Link href={"/IMC"}>IMC</Link>
-          <Link href={"/ano"}>Descubra seu Ano</Link>
-          <Link href={"/cadastro"}>Cadastro</Link>
+        <div className="  text-3xl  flex flex-col row-span-1 p-5">
+          <Link href={"/IMC"}> App IMC</Link>
+            <Link href={"/ano"}>Descubra seu Ano</Link>
+              <Link href={"/cadastro"}>Cadastro</Link>
         </div>
-        <div className="bg-black flex items-center justify-center row-span-1  ">
-          <div className=" flex-col w-120 items-center flex justify-center h-100">
-            <h1 className="text-orange-400 text-4xl">BEM-VINDO!</h1>
-            <p className="text-blue-500">Aplicativos Web do Curso do Senac 2026</p>
-            <p>Turma 027</p>
+
+        <div className=" bg-cover bg-center h-screen flex items-center justify-center">
+          <div className="  mr-[14vw] flex-col w-120 items-center flex justify-center h-100">
+            <h1 className="text-orange-600 font-black animate-bounce -tracking-tight text-4xl">BEM VINDO !</h1>
+            <p className="text-black font-bold">Aplicativos Web do Curso do Senac 2026</p>
+            <p className="font-bold">Turma 027</p>
           </div>
         </div>
+
       </div>
-
-
 
 
     </div>
