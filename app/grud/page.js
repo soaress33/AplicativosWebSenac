99d32b3b85e91c18e1f3ce00adde3d1cada0c16e
Junior de Oliveira Soares <1036603@senacminas.edu.br>
2026-04-17 17:17:41 -0300

    cadastro

--- app/grud/page.js
+++ app/grud/page.js
@@ -5,62 +5,76 @@ import { LucideClipboardList } from "lucide-react";
 
 
 export default function Home() {
+
+    const [alunos, setAlunos] = useState([]);
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [celular, setCelular] = useState("");
+    const [curso, setCurso] = useState("");
+
+    const [editIndex, setEditIndex] = useState(null);
+    
+
+
+
+
     return (
-        <div className="bg-white items-center flex justify-center w-screen h-screen">
-            <div className="bg-gray-300 items-center flex flex-col rounded-2xl w-200 h-174">
-                <div className=" mt-3   -ml-100 flex">
-                    <div className="bg-blue-200 rounded-2xl ">
-                        <LucideClipboardList className="h-12 w-auto text-[#4559D9]"></LucideClipboardList>
-                    </div>
-                    <div className="ml-3">
-                        <h1 className="text-3xl font-bold text-black">Minhas Tarefas</h1>
-                        <p className="-mt-2 text-gray-500">Gerencie suas tarefas de formas simples</p>
-                    </div>
-                </div>
-                <div className="bg-white w-185 border rounded-2xl mt-5 border-gray-300 h-90">
-                    <h1 className="text-black ml-2 mt-2 text-2xl">Nova tarefa</h1>
-                    <div className=" text-black ml-2 mt-3">
-                        <h1>Tarefas</h1>
-                        <input className="w-180 h-11 border-black rounded border"></input>
-                    </div>
-
-                    <div className=" flex   text-black ml-2 mt-3">
-                        <div className="">
-                            <h1>Data</h1>
-                            <input
-                                type="date"
-                                placeholder="dd/mm/aaaa"
-                                className="w-86 h-11 p-2 rounded border-black border"></input>
-                        </div>
-
-                        <div className="ml-8 ">
-                            <h1>Horário</h1>
-                            <input
-                                type="time"
-                                placeholder="--:--"
-                                className="w-86 h-11 p-2 rounded border-black border"></input>
-                        </div>
-                    </div>
-
-
-                    <div className=" text-black ml-2 mt-3">
-                        <h1>Status</h1>
-                        <input className="w-180 h-11 rounded border-black border">
-
-
-                        </input>
-                    </div>
-                    <button className="bg-[#3B57DD] w-40 h-10 flex items-center justify-center hover:bg-blue-500 mt-4 ml-2 rounded">
-                        <p >+</p>
-                        <p className="ml-3">Adicionar</p>
-                    </button>
+        <div className="bg-black items-center flex-col flex justify-center w-screen h-screen">
+            <div className="bg-black w-190 items-center flex flex-col rounded-2xl ">
+                <div className="mt-2 items-center justify-center flex-col flex text-3xl font-black ">
+                    <h1>Cadastro de Alunos Senac Competição</h1>
+                    <p className="text-orange-500 text-xl">Etapa Escolar Lavras-MG</p>
                 </div>
 
-                <div className="bg-white w-185 mt-4 rounded-2xl h-50"></div>
+                <div className="justify-center  fle mt-5 flex">
+                    <div className="">
+                        <input
+                            placeholder="Nome"
+                            className="border-2 me-1 p-1 rounded h-10 w-92"></input>
 
+                        <input
+                            placeholder="Email"
+                            className="border-2 p-1 ml-1 rounded h-10 w-92"></input>
+                    </div>
+                </div>
+
+                <div className="justify-center  fle mt-3 flex">
+                    <div className="">
+                        <input
+                            placeholder="Celular"
+                            className="border-2 me-1 p-1 rounded h-10 w-92"></input>
+
+                        <input
+                            placeholder="Curso"
+                            className="border-2 p-1 ml-1 rounded h-10 w-92"></input>
+                    </div>
+                </div>
+                <button className="bg-blue-500 hover:bg-blue-400 mt-4  w-185 rounded  h-12">Cadastrar</button>
+
+                {/**Tabela */}
+                <div className=" w-190 mt-4 flex p-2 pl-2 items-center justify-center ">
+                    <table className="w-full ">
+                        <tr className="w-full h-10 justify-center border-white border-2  bg-amber-50  items-center font-black text-black">
+                            <td className="text-center">Nome</td>
+                            <td className="text-center">Email</td>
+                            <td className="text-center">Celular</td>
+                            <td className="text-center">Curso</td>
+                            <td className="text-center">Ações</td>
+                        </tr>
+
+                        <tr className="w-full h-10 justify-center ml-10      border bg-black items-center font-black text-white">
+                            <td className="text-center border">Nome</td>
+                            <td className="text-center border">Email</td>
+                            <td className="text-center border">Celular</td>
+                            <td className="text-center border">Ações</td>
+                            <td className="flex items-center justify-center mt-2     gap-2">
+                                <button className="bg-yellow-500 hover:bg-yellow-400 rounded w-18">Editar</button>
+                                <button className="bg-red-500 hover:bg-red-400  rounded w-18">Deletar</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-
-
         </div>
     );
 }
